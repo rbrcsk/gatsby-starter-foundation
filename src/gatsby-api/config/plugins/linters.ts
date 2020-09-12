2f0657d1fcbae4fd54cc9b8c9f5ad8fc27370635
Richard Borcsik <borcsikrichard@gmail.com>
2020-09-12 18:50:02 +0200

    Improve linting setup

--- src/gatsby-api/config/plugins/linters.ts
+++ src/gatsby-api/config/plugins/linters.ts
@@ -1,1 +1,19 @@
-export const linterPlugins = []
+const styleLint = {
+  resolve: "@danbruegge/gatsby-plugin-stylelint",
+  options: { files: ["**/*.{js,jsx,ts,tsx}"] },
+}
+
+const esLint = {
+  resolve: "gatsby-plugin-eslint",
+  options: {
+    test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+    exclude: /(node_modules|.cache|public)/,
+    stages: ["develop", "build-javascript"],
+    options: {
+      emitWarning: true,
+      failOnError: true,
+    },
+  },
+}
+
+export const linterPlugins = [styleLint, esLint]
